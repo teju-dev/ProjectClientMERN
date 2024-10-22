cc9d1ad2887455a02cdbeb924540cb87d7133882
Teju <dev.teju8@gmail.com>
2024-10-22 18:01:47 -0400

    move index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const colors = require('colors');
 const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
-const connectDB = require('./config/db');
+const schema = require('./server/schema/schema');
+const connectDB = require('./server/config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
